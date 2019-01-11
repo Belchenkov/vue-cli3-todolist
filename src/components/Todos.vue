8d623e1b1fd2757c3786abdc99cc1e6d5b2f1669
Aleksey Belchenkov <u608110@gmail.com>
2019-01-11 22:02:38 +0300

    Output Todos in List

---
+++ src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div :key="todo.id" v-for="todo in todos">
+            <TodoItem :todo="todo" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './TodoItem';
+
+    export default {
+        props: [
+            'todos'
+        ],
+        name: "Todos",
+        components: {
+            TodoItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
